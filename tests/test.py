fab1ae2becec1601ae5e59ce6bd63ee0b1fe2955
Kuriiios <cyril.leconte07@gmail.com>
2025-11-18 23:22:10 +0100

    cleaned unused part of code

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,4 @@
-import os
-import sys
 from app.components.zorglangue_translator import zorglangue_translator 
-
-sys.path.insert(0,
-                os.path.abspath(
-                    os.path.join(
-                        os.path.dirname(__file__),
-                        ".."
-                    ) ))
 
 def test_zorglangue_translator():
     assert zorglangue_translator('Vive Zorglub !') == 'eviV bulgroZ !'
